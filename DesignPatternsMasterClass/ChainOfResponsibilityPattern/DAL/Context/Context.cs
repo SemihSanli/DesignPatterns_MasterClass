31fcadbd0ef200c8c076d9b79c92b425a2fd0fbf
Semih Şanlı <semih-semih-35@hotmail.com>
2025-10-27 16:03:03 +0300

    ChainOfResponsibility
    ChainOfResponsibility Design Pattern

---
+++ DesignPatternsMasterClass/ChainOfResponsibilityPattern/DAL/Context/Context.cs
@@ -0,0 +1,14 @@
+﻿using ChainOfResponsibilityPattern.DAL.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace ChainOfResponsibilityPattern.DAL.Context
+{
+    public class Context:DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+           optionsBuilder.UseSqlServer("server=DESKTOP-33VDDOP\\SQLEXPRESS17;database=ChainOfResponsibilityPatternDB;integrated security=true;trustServerCertificate=true;");
+        }
+        public DbSet<CustomerProcess> CustomerProcesses { get; set; }
+    }
+}
---
+++ DesignPatternsMasterClass/ChainOfResponsibilityPattern/DAL/Entities/CustomerProcess.cs
@@ -0,0 +1,11 @@
+﻿namespace ChainOfResponsibilityPattern.DAL.Entities
+{
+    public class CustomerProcess
+    {
+        public int CustomerProcessId { get; set; }
+        public string FullName { get; set; }
+        public string Amount { get; set; }
+        public string EmployeeName { get; set; }
+        public string Description { get; set; }
+    }
+}
